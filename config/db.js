e0eed207c57b82508ad43ae612b57a22a25b8a58
sancelaa <sanela.mandal@gmail.com>
2020-12-09 20:49:51 +0100

    database config

--- config/db.js
+++ config/db.js
@@ -1,1 +1,10 @@
 const {Pool} = require('pg');
+const db = new Pool({
+    host:'hattie.db.elephantsql.com',
+    password:'3VxgmRsiTt1qEZkMYFT0yExWrzDJ_sig',
+    user: 'hiogtygx',
+    port: 5432,
+    database:'hiogtygx'
+})
+
+module.exports = pg
